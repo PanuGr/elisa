149c129e86e0e84c538dfea2c2e4182bb14a8377
PanuGr <ppanagiotisilv@hotmail.com>
2021-01-07 13:30:35 +0200

    new page for each book

--- assets/articles.js
+++ assets/articles.js
@@ -1,16 +1,23 @@
 const content = {
-    content: [{
-        title: 'History',
-        description: 'Kirjan kuvaus',
-        date: '7.01.2021',
-        imgSrc:'background/felipepelaquim-InpSZCpwS7g-unsplash.webp',
-        link:''
-    },
-    {
-        title: 'art',
-        description: 'Kirjan kuvaus',
-        date: '17.01.2021',
-        imgSrc:'background/joceline-m-huitzil-3bX5yrvFgKk-unsplash.webp',
-        link:''
-    }]
+    content: [
+        {
+            id: 0,
+            title: 'History',
+            description: 'Kirjan kuvaus',
+            date: '7.01.2021',
+            imgSrc: 'background/felipepelaquim-InpSZCpwS7g-unsplash.webp',
+            link: '',
+
+            text: 'aaaaaaa'
+        },
+        {
+            title: 'art',
+            description: 'Kirjan kuvaus',
+            date: '17.01.2021',
+            imgSrc: 'background/joceline-m-huitzil-3bX5yrvFgKk-unsplash.webp',
+            link: '',
+            id: 1,
+            text: 'ssssssss'
+        }
+    ]
 };
--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-//SHARE
-var a2a_config = a2a_config || {};
-a2a_config.locale = "fi";
-a2a_config.num_services = 4;
-
-//HANDLEBARS
-const source = document.getElementById("template").innerHTML; //get the template
-const templateBlog = Handlebars.compile(source);
-const compiledHtml = templateBlog(content); //fill the template with our info
-const fill = document.getElementsByTagName("section")[0]; // add the filled template in destination
-fill.innerHTML = compiledHtml;
-
-const source2 = document.getElementById("template2").innerHTML;
-const blogList = Handlebars.compile(source2);
-document.getElementsByTagName("ul")[0].innerHTML = blogList(content)
-
-
 const backgroundUrl = [
     "felipepelaquim-InpSZCpwS7g-unsplash.webp", 'annie-spratt-nCn9gE5-1wU-unsplash.webp', 'evie-s-SxEhEk9O2H0-unsplash.webp',
     'joceline-m-huitzil-3bX5yrvFgKk-unsplash.webp', 'oleksandra-bardash-6rRLtMcEnzg-unsplash.webp', 'pawel-czerwinski-giIUmmihDFA-unsplash.webp',
@@ -24,4 +7,30 @@ const backgroundUrl = [
 const randomNum = () => {
     return Math.floor(Math.random() * backgroundUrl.length)
 }
-document.getElementsByTagName('body')[0].style.backgroundImage = `url(/assets/background/${backgroundUrl[randomNum()]} ) ` 
+
+//HANDLEBARS
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const articleID = urlParams.get('id')
+
+
+const source = document.getElementById("template").innerHTML; //get the template
+const templateBlog = Handlebars.compile(source);
+const compiledHtml = templateBlog(content); //fill the template with our info
+const fill = document.getElementsByTagName("section")[0]; // add the filled template in destination
+fill.innerHTML = compiledHtml;
+if (document.getElementById("template2")) {
+    const source2 = document.getElementById("template2").innerHTML;
+    const blogList = Handlebars.compile(source2);
+    document.getElementsByTagName("ul")[0].innerHTML = blogList(content)
+}
+
+//SHARE
+var a2a_config = a2a_config || {};
+a2a_config.locale = "fi";
+a2a_config.num_services = 4;
+
+//RANDOM BACKGROUND
+document.getElementsByTagName('body')[0].style.backgroundImage = `url(/assets/background/${backgroundUrl[randomNum()]} ) `
+
+
