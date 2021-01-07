2f3680e9687a05ec113cdff6a09c95735b8d5ed7
PanuGr <ppanagiotisilv@hotmail.com>
2021-01-07 12:18:42 +0200

    fix style

---
+++ assets/articles.js
@@ -0,0 +1,16 @@
+const content = {
+    content: [{
+        title: 'History',
+        description: 'Kirjan kuvaus',
+        date: '7.01.2021',
+        imgSrc:'background/felipepelaquim-InpSZCpwS7g-unsplash.webp',
+        link:''
+    },
+    {
+        title: 'art',
+        description: 'Kirjan kuvaus',
+        date: '17.01.2021',
+        imgSrc:'background/joceline-m-huitzil-3bX5yrvFgKk-unsplash.webp',
+        link:''
+    }]
+};
--- main.js
+++ main.js
@@ -4,9 +4,24 @@ a2a_config.locale = "fi";
 a2a_config.num_services = 4;
 
 //HANDLEBARS
-const blogEntries = document.getElementById("").innerHTML; //get the template
-const templateBlog = Handlebars.compile(blogEntries);
-const content = {};
-const compiledHtml = template(content); //fill the template with our info
-const fill = document.getElementById(""); // add the filled template in destination
+const source = document.getElementById("template").innerHTML; //get the template
+const templateBlog = Handlebars.compile(source);
+const compiledHtml = templateBlog(content); //fill the template with our info
+const fill = document.getElementsByTagName("section")[0]; // add the filled template in destination
 fill.innerHTML = compiledHtml;
+
+const source2 = document.getElementById("template2").innerHTML;
+const blogList = Handlebars.compile(source2);
+document.getElementsByTagName("ul")[0].innerHTML = blogList(content)
+
+
+const backgroundUrl = [
+    "felipepelaquim-InpSZCpwS7g-unsplash.webp", 'annie-spratt-nCn9gE5-1wU-unsplash.webp', 'evie-s-SxEhEk9O2H0-unsplash.webp',
+    'joceline-m-huitzil-3bX5yrvFgKk-unsplash.webp', 'oleksandra-bardash-6rRLtMcEnzg-unsplash.webp', 'pawel-czerwinski-giIUmmihDFA-unsplash.webp',
+    'priscilla-du-preez-5mgtTFa7Y3M-unsplash.webp', 'priscilla-du-preez-K1-mGLwGJJE-unsplash.webp', 'priscilla-du-preez-KeUHnTJ78fY-unsplash.webp',
+    'priscilla-du-preez-yS13_sGYElg-unsplash.webp', 'zoltan-tasi-vds0LlvYe58-unsplash.webp'
+];
+const randomNum = () => {
+    return Math.floor(Math.random() * backgroundUrl.length)
+}
+document.getElementsByTagName('body')[0].style.backgroundImage = `url(/assets/background/${backgroundUrl[randomNum()]} ) ` 
