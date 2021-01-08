a5864c7b8e5472a47c160e97f3b9c4745a93de04
PanuGr <ppanagiotisilv@hotmail.com>
2021-01-08 12:13:01 +0200

    final

--- assets/articles.js
+++ assets/articles.js
@@ -4,11 +4,10 @@ const content = {
             title: 'A',
             description: 'Elisa Stenberg, Hyvän Kukkia, 2009, Jamilahden kansanopisto',
             date: '7.01.2021',
-            imgSrc: 'background/felipepelaquim-InpSZCpwS7g-unsplash.webp',
+            imgSrc: 'bookCover/A.png',
             link: '',
             text: "Valkoisen paperin edessä, <br> olen kirjoittanut jo rivin,<br> mutta kirjaimet eivät näy,<br> olen varmasti sokea, <br>tästä maailmasta, <br>en kuule enkä haista,<br> A-kirjamen sellissä,<br> nälkäisenä,<br> itseni seuralaisena,<br> pidän tiimalasia kädessä. "
         },
     ]
 };
 
-let shareUrl =`https://${window.location.host}/${window.location.pathname}${window.location.search } ` ; 
