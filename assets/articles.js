d6a3e056b99168c547af5ff73ae88f83de721338
PanuGr <ppanagiotisilv@hotmail.com>
2021-01-10 19:22:27 +0200

    updates

--- assets/articles.js
+++ assets/articles.js
@@ -6,6 +6,7 @@ const content = {
             date: '7.01.2021',
             imgSrc: 'bookCover/A.webp',
             mobile: 'bookCover/A540px.webp',
+            imageComment:'Flower in Uki',
             link: '',
             text: "Valkoisen paperin edessä, <br> olen kirjoittanut jo rivin,<br> mutta kirjaimet eivät näy,<br> olen varmasti sokea, <br>tästä maailmasta, <br>en kuule enkä haista,<br> A-kirjamen sellissä,<br> nälkäisenä,<br> itseni seuralaisena,<br> pidän tiimalasia kädessä. "
         },
