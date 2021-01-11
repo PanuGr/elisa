9e7de5e65a84b2433df866a46648f1c7aaf33df9
PanuGr <ppanagiotisilv@hotmail.com>
2021-01-11 10:39:10 +0200

    responsive img

--- assets/articles.js
+++ assets/articles.js
@@ -5,7 +5,7 @@ const content = {
             description: 'Elisa Stenberg, Hyvän Kukkia, 2009, Jamilahden kansanopisto',
             date: '7.01.2021',
             imgSrc: 'bookCover/A.webp',
-            mobile: 'bookCover/A540px.webp',
+            imageSmall: 'bookCover/A540px.webp',
             imageComment:'Auringonkukka, Uusikaupunki, 09/2020',
             link: '',
             text: "Valkoisen paperin edessä, <br> olen kirjoittanut jo rivin,<br> mutta kirjaimet eivät näy,<br> olen varmasti sokea, <br>tästä maailmasta, <br>en kuule enkä haista,<br> A-kirjamen sellissä,<br> nälkäisenä,<br> itseni seuralaisena,<br> pidän tiimalasia kädessä. "
